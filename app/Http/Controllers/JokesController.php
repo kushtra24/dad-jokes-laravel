2f22a7e8a2f0418abef7103d274dfd6c8a7d4762
kushtra24 <kushtra24@gmail.com>
2020-10-12 13:04:17 +0200

    get jokes return a json response

--- app/Http/Controllers/JokesController.php
+++ app/Http/Controllers/JokesController.php
@@ -1,10 +1,32 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Joke;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class JokesController extends Controller
 {
-    //
+    public function index() {
+        $jokes = $this->getJokes();
+
+        return response()->json($jokes, 200);
+    }
+
+    public function getJokes() {
+        $client = new Client(['headers' => ['Accept' => 'application/json']]);
+        $res = $client->request('GET', 'https://icanhazdadjoke.com/search');
+        $data = json_decode($res->getBody()->getContents(), true);
+        return $data;
+    }
+
 }
+
+
+
+//$data = json_decode($res->getBody()->getContents(),true);
+//$events = $data['Data'];
+//foreach($events as $item)
+//{
+//    DB::table('your_table')->insert(['timeLive'=>$item['timeLive']])
+//}
