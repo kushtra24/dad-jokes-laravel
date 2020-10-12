9cadcbb98f3a3924423a29039479d9a8f397586d
kushtra24 <kushtra24@gmail.com>
2020-10-12 20:49:09 +0200

    limit and search term

--- app/Http/Controllers/JokesController.php
+++ app/Http/Controllers/JokesController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Joke;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -11,19 +10,29 @@ class JokesController extends Controller
     public function index(Request $request) {
 
         $page = $request->input('current_page', null);
+        $limit = $request->input('limit', null);
+        $searchTerm = $request->input('term', null);
 
-        $jokes = $this->getJokes($page);
-
-        $result = $jokes;
+        $jokes = $this->getJokes($page, $limit, $searchTerm);
 
         return response()->json($jokes, 200);
     }
 
-    public function getJokes($page) {
+    public function getJokes($page, $limit, $searchTerm) {
         $client = new Client(['headers' => ['Accept' => 'application/json']]);
-        $res = $client->request('GET', 'https://icanhazdadjoke.com/search?page=' . $page);
-        $data = json_decode($res->getBody()->getContents(), true);
-        return $data;
+        $url = 'https://icanhazdadjoke.com/search';
+        if (isset($page)) {
+            $url .= '?page=' . $page;
+        }
+        if (isset($searchTerm)) {
+            $url .= '&term=' . $searchTerm;
+        }
+        if (isset($limit)) {
+            $url .= '&limit=' . $limit;
+        }
+        Log::info(var_export($url, true));
+        $res = $client->request('GET', $url);
+        return json_decode($res->getBody()->getContents(), true);
     }
 
 }
